f2a7e86cd00f9f1fc8a27e1c07c3137e1a128f3e
Gabriel West <s1365736@arkaig.inf.ed.ac.uk>
2013-09-27 17:25:16 +0100

    part 1 of task 1 is working at this commit

--- robot.py
+++ robot.py
@@ -16,7 +16,7 @@ def load():
 
 def start(r):
 	try:
-		max_ir_reading = .4 # This represents the min distance
+		max_ir_reading = 100 # This represents the min distance
 		prev_l = 5
 		prev_r = 5
 		r.turn(prev_l,prev_r)
@@ -24,25 +24,21 @@ def start(r):
 			vl = 5
 			vr = 5
 			vals = r.get_values('n')
-			print 'normalizing values'
-			normalized_vals = map(normalize_IR, vals)
-			print normalized_vals
-
 			print 'IR readings: %s' % vals
 
-			if(normalized_vals[1] > max_ir_reading):
-				vr -= (2 * int(vl * normalized_vals[1]))
-			if(normalized_vals[2] > max_ir_reading):
-				vr -= (2 * int(vl * normalized_vals[2]))
-			if(normalized_vals[3] > max_ir_reading):
-				vl -= (2 * int(vr * normalized_vals[3]))
-			if(normalized_vals[4] > max_ir_reading):
-				vl -= (2 * int(vr * normalized_vals[4]))
+			if(vals[1] > max_ir_reading):
+				vr -= 4
+			if(vals[2] > max_ir_reading):
+				vr -= 4
+			if(vals[3] > max_ir_reading):
+				vl -= 4
+			if(vals[4] > max_ir_reading):
+				vl -= 4
 			if((vl == vr) and vl != 5):
 				vl = -vl
 			if (vl, vr) != (prev_l, prev_r):
 				print 'setting speed to: left: %s, right: %s' % (vl, vr)
-				# r.turn(vl,vr)
+				r.turn(vl,vr)
 				prev_l = vl
 				prev_r = vr
 
