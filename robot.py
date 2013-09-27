5ed362bfb765b2d5f5ba5ca0218c3cd593228481
Andr??s Lozano fern??ndez <s1368687@awe.inf.ed.ac.uk>
2013-09-27 11:53:30 +0100

    At lab on friday

--- robot.py
+++ robot.py
@@ -7,7 +7,23 @@ import pykhepera
 
 
 def load():
+	mind = 60 ''' This is the minimum distance to turn away from an object'''
 	reload(pykhepera)
 	r = pykhepera.PyKhepera()
+	while(1):
+		vl = 5
+		vr = 5
+		val = get_values(n)
+		if(val[1] < mind):
+			--vr
+		if(val[2] < mind):
+			--vr
+		if(val[3] < mind):
+			--vl
+		if(val[4] < mind):
+			--vr
+		if(vl == vr):
+			vl = -vl
+		r.set_speed(vl,vr)
 	return r
 
