2cd223f29d36406e4ccb2250ec8b24e8258289da
Gabriel West <s1365736@langdon.inf.ed.ac.uk>
2013-10-15 13:35:45 +0100

    plotting works, worth 1 point!

--- robot.py
+++ robot.py
@@ -1,218 +1,199 @@
-'''This is the logic/execution module for the pykhepera robot. It handles the
-high level functions such as reloading the robot, controlling the robot,
-etc.
-'''
 
-import serial
+
 import pykhepera, behaviors
 from data import Data
 import time
 import matplotlib.pyplot as plt
 import numpy as np
 import pdb
+import threading
 
-def update_data():
-    data.sensor_values = r.read_sensor_values()
-    data.wheel_values = aux = r.read_wheel_values()
+class Robot(object):
+    '''This is the logic/execution module for the pykhepera robot. It handles the
+    high level functions such as reloading the robot, controlling the robot,
+    etc.
+    '''
+    def __init__(self):
+        self.data = Data()
+        plt.ion()
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(1,1,1)
+        self.ax.axis([-1000, 1000, -1000, 1000])
+        self.line, = self.ax.plot(self.data.x_positions, self.data.y_positions)
+        self.r = pykhepera.PyKhepera()
+        self.axel_l = 53.0 #self.axis length in mm
+        self.running = True
 
-def restart():
-    r = load()
-    start(r)
+    def update_data(self):
+        self.data.sensor_values = self.r.read_sensor_values()
+        self.data.wheel_values = self.r.read_wheel_values()
 
-def load():
-    reload(pykhepera)
-    r = pykhepera.PyKhepera()
-    r.reset_wheel_counters()
-    return r
+    def to_mm(self, wheel_value):
+        return float(wheel_value * 0.08)
 
-def to_mm(wheel_value):
-    return float(wheel_value * 0.08)
+    def to_wu(self, mm):
+        return int(mm/0.08)
 
-def to_wu(mm):
-    return int(mm/0.08)
+    def get_omega(self):
+        vl, vr = self.data.wheel_speeds
+        if vr == vl:
+            return 0
+        omega = ((vr - vl)/self.axel_l)
+        return omega
 
-def get_omega():
-    vl, vr = data.wheel_speeds
-    if vr == vl:
-        return 0
-    omega = ((vr - vl)/axis_l)
-    return omega
+    def get_R(self):
+        vl, vr = self.data.wheel_speeds
+        if vl == vr:
+            return 0
+        R = (self.axel_l/2) * ((vl + vr)/(vr - vl))
+        return R
 
-def get_R():
-    vl, vr = data.wheel_speeds
-    if vl == vr:
-        return 0
-    R = (axis_l/2) * ((vl + vr)/(vr - vl))
-    return R
+    def get_ICC(self):
+        x = self.data.x_positions[-1]
+        y = self.data.y_positions[-1]
+        R = self.get_R()
+        theta = self.data.theta
 
-def get_ICC():
-    x = data.x_positions[-1]
-    y = data.y_positions[-1]
-    R = get_R()
-    theta = data.theta
+        ICC = ((x - (R * np.sin(theta))), (y + (R * np.cos(theta))))
+        return ICC
 
-    ICC = ((x - (R * np.sin(theta))), (y + (R * np.cos(theta))))
-    return ICC 
+    def calculate_pose(self, dt):
+        x = self.data.x_positions[-1]
+        y = self.data.y_positions[-1]
+        theta = self.data.theta
 
-def calculate_pose(dt):
-    x = data.x_positions[-1]
-    y = data.y_positions[-1]
-    theta = data.theta
+        vl, vr = self.data.wheel_speeds
+        if vl == vr:
+            pose = (np.array([x, y, theta]))
+            translation = np.array([vl*np.cos(theta) * dt,
+                                   vl*np.sin(theta) * dt, 0])
+            pose = pose + translation
+        else:
+            ICC = self.get_ICC()
+            ICCx = ICC[0]
+            ICCy = ICC[1]
+            omega = self.get_omega()
 
-    vl, vr = data.wheel_speeds
-    if vl == vr:
-        pose = (np.array([x, y, theta]))
-        translation = np.array([vl*np.cos(theta), vl*np.sin(theta), 0])
-        pose = pose + translation
-    else:
-        R = get_R()
-        ICC = get_ICC()
-        ICCx = ICC[0]
-        ICCy = ICC[1]
-        omega = get_omega()
+            rotation_matrix = np.array(
+                [
+                [np.cos(omega * dt), -np.sin(omega*dt), 0],
+                [np.sin(omega * dt), np.cos(omega * dt), 0],
+                [0,                  0,                  1]
+                ])
 
-        rotation_matrix = np.array(
-            [
-            [np.cos(omega * dt), -np.sin(omega*dt), 0],
-            [np.sin(omega * dt), np.cos(omega * dt), 0],
-            [0,                  0,                  1]
-            ])
+            ICC_vector = np.array([x - ICCx, y-ICCy, theta])
 
-        ICC_vector = np.array([x - ICCx, y-ICCy, theta])
-
-        reposition_vector = np.array([ICCx, ICCy, omega * dt])
-
-
-        pose = np.dot(rotation_matrix, ICC_vector) + reposition_vector
-
-    data.x_positions.append(pose[0])
-    data.y_positions.append(pose[1])
-    data.theta = pose[2]
-
-
-
-def calibrate():
-    update_data()
-    r.turn((1,-4))
-    while (data.wheel_values[0] + data.wheel_values[1]) < 2078:
-        update_data()
-    r.turn((0,0))
+            reposition_vector = np.array([ICCx, ICCy, omega * dt])
 
 
-def calibrate_min():
-    r.reset_wheel_counters()
-    update_data()
-    mins = data.sensor_values
-    r.turn((5,-5))
-    while data.wheel_values[0] <  2000:
-        update_data()
-        aux = data.sensor_values
-        for i, val in enumerate(aux):
-            if val > mins[i]:
-                mins[i] = val
-    r.turn((0,0))
-    m = 0
-    for val in mins:
-        m += val
-    m = m/8
-    data.thresholds['max_ir_reading'] = m
-    data.thresholds['wall_max'] = m
-    if m:
-        print 'calibration succesful: ', mins
-        fout = open('data_calibration.data', 'a')
-        fout.write('%d:%s' % (m, ''.join(str(mins))))
-        fout.write('\n')
-        fout.close()
+            pose = np.dot(rotation_matrix, ICC_vector) + reposition_vector
+
+        self.data.x_positions.append(pose[0])
+        self.data.y_positions.append(pose[1])
+        self.data.theta = pose[2]
 
 
-def update_plot():
-    line.set_xdata(data.x_positions)
-    line.set_ydata(data.y_positions)
-    fig.canvas.draw()
 
-def print_trajectory():
-    _trajectory_x.append(data.wheel_values[0])
-    _trajectory_y.append(data.wheel_values[1])
-    #print _trajectory_x
-    #print _trajectory_y
-    ax.clear()
-    ax.bar(_trajectory_x,_trajectory_y)
-    plt.draw()
-    plt.show()
+    def calibrate(self):
+        self.update_data()
+        self.r.turn((1,-4))
+        while (self.data.wheel_values[0] + self.data.wheel_values[1]) < 2078:
+            self.update_data()
+        self.r.turn((0,0))
 
-def will_collide():
-    for val in data.sensor_values[1:5]:
-        if val > data.thresholds['max_ir_reading']:
-            return True
 
-def state_LED(number):
-    '''converts a number to binary and lights LED accordingly'''
-    if number > 3:
-        return
-    binary = bin(number)[2:]
-    if len(binary) == 1:
-        binary = '0' + binary
-    for i, bit in enumerate(binary):
-        r.led(i, bit)
+    def calibrate_min(self):
+        self.r.reset_wheel_counters()
+        self.update_data()
+        mins = self.data.sensor_values
+        self.r.turn((5,-5))
+        while self.data.wheel_values[0] <  2000:
+            self.update_data()
+            aux = self.data.sensor_values
+            for i, val in enumerate(aux):
+                if val > mins[i]:
+                    mins[i] = val
+        self.r.turn((0,0))
+        m = 0
+        for val in mins:
+            m += val
+        m = m/8
+        self.data.thresholds['mself.ax_ir_reading'] = m
+        self.data.thresholds['wall_mself.ax'] = m
+        if m:
+            print 'calibration succesful: ', mins
+            fout = open('data_calibration.data', 'a')
+            fout.write('%d:%s' % (m, ''.join(str(mins))))
+            fout.write('\n')
+            fout.close()
 
-def start():
-    r = load()
-    obj_avoid = behaviors.ObjAvoid(data)
-    wall_follow = behaviors.WallFollow(data)
-    last_time = time.time()
-    try:
-        while 1:
-            dt = (time.time() - last_time)
-            last_time = time.time()
-            update_data()
-            calculate_pose(dt)
-            update_plot()
 
-            speed = (0,0)
-            vals = data.sensor_values
-            if r.state is 0:
-                state_LED(0)
-                if will_collide():
-                    speed = (0,0)
-                    r.state = 1
-                else:
-                    speed = (5,5)
-            if r.state is 1:
-                state_LED(1)
-                speed = obj_avoid.step()
-                if speed == (5, 5):
-                    r.state = 2
-                    wall_follow.prev_vals = vals
-                    speed = (0,0)
-            elif r.state is 2:
-                state_LED(2)
-                for val in vals[2:3]: #Am I close on the front sensors?
-                    if val > data.thresholds['max_ir_reading']:
-                        #print "WALL!"
-                        r.state = 1
+    def update_plot(self):
+        self.line.set_xdata(self.data.x_positions)
+        self.line.set_ydata(self.data.y_positions)
+        self.fig.canvas.draw()
+
+    def will_collide(self):
+        for val in self.data.sensor_values[1:5]:
+            if val > self.data.thresholds['max_ir_reading']:
+                return True
+
+    def state_led(self, number):
+        '''converts a number to binary and lights LED accordingly'''
+        if number > 3:
+            return
+        binary = bin(number)[2:]
+        if len(binary) == 1:
+            binary = '0' + binary
+        for i, bit in enumerate(binary):
+            self.r.led(i, bit)
+
+    def start(self):
+        obj_avoid = behaviors.ObjAvoid(self.data)
+        wall_follow = behaviors.WallFollow(self.data)
+        last_time = time.time()
+        try:
+            while self.running:
+                dt = (time.time() - last_time)
+                last_time = time.time()
+                self.update_data()
+                self.calculate_pose(dt)
+                self.update_plot()
+
+                speed = (0,0)
+                vals = self.data.sensor_values
+                if self.r.state is 0:
+                    self.state_led(0)
+                    if self.will_collide():
+                        speed = (0,0)
+                        self.r.state = 1
+                    else:
+                        speed = (5,5)
+                if self.r.state is 1:
+                    self.state_led(1)
+                    speed = obj_avoid.step()
+                    if speed == (5, 5):
+                        self.r.state = 2
+                        wall_follow.prev_vals = vals
+                        speed = (0,0)
+                elif self.r.state is 2:
+                    self.state_led(2)
+                    if self.will_collide():
+                        self.r.state = 1
                         continue
-                if vals[0] < data.thresholds['wall_min'] and vals[5] < data.thresholds['wall_min']:
-                    #print "No wall :( "
-                    r.state = 0
-                    continue
-                speed = wall_follow.step()
-                #print "Following :)"
-            if speed:
-                data.wheel_speeds = speed
-                r.turn(speed)
-    except KeyboardInterrupt:
-        print 'killing and cleaning up'
-        r.purge_buffer()
-        state_LED(0)
-        r.stop()
-        r.kill()
-
-data = Data()
-plt.ion()
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-ax.axis([-3000, 3000, -3000, 3000])
-line, = ax.plot(data.x_positions, data.y_positions)
-
-r = load()
-axis_l = 53.0 #axis length in mm
+                    if (vals[0] < self.data.thresholds['wall_min']
+                        and vals[5] < self.data.thresholds['wall_min']):
+                        #"No wall :( "
+                        self.r.state = 0
+                        continue
+                    speed = wall_follow.step()
+                    #print "Following :)"
+                if speed:
+                    self.data.wheel_speeds = speed
+                    self.r.turn(speed)
+        except KeyboardInterrupt:
+            print 'killing and cleaning up'
+            self.r.purge_buffer()
+            self.state_led(0)
+            self.r.stop()
+            self.r.kill()
